a6dd62e14c3a04fb5e241f490e591bfae5bf4727
Renzo Salcedo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-06-17 20:57:50 -0400

    ajout fct processus_parent

--- echelon.c
+++ echelon.c
@@ -33,12 +33,32 @@ int trouver_numero_signal(int *numero_signal, int status) {
     return *numero_signal;
 }
 
-int
-main(int argc, char *argv[]) {
-    int status;
+int executer_processus_parent(int status, int *compteur1, int *compteur2, pid_t pid_enfant) {
+    int numero_signal = 0;
+    while (1) {
+        pid_t pid_valide = wait(&status);
+        ptrace(PTRACE_SETOPTIONS, pid_enfant, 0,
+               PTRACE_O_EXITKILL | PTRACE_O_TRACEEXEC | PTRACE_O_TRACEFORK | PTRACE_O_TRACECLONE);
+        incrementer_compteurs(compteur1, compteur2, status);
+        numero_signal = trouver_numero_signal(&numero_signal, status);
+        if (WIFEXITED(status) && pid_valide == pid_enfant) {
+            printf("%d\n", *compteur2);
+            printf("%d", *compteur1);
+            return WEXITSTATUS (status);
+        } else if (WIFSIGNALED(status)) {
+            printf("%d\n", *compteur2);
+            printf("%d", *compteur1);
+            return 128 + WTERMSIG(status);
+        }
+        ptrace(PTRACE_CONT, pid_valide, 0, numero_signal);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    int status = 0;
     int compteur1 = 0;
     int compteur2 = 0;
-    int numero_signal = 0;
     pid_t pid_enfant = fork();
     if (pid_enfant < 0)
         exit(1);
@@ -46,23 +66,8 @@ main(int argc, char *argv[]) {
         ptrace(PTRACE_TRACEME, pid_enfant, NULL, NULL);
         raise(SIGSTOP);
         execve(argv[1], &argv[1], NULL);
-        return  1;
+        return 1;
     } else {
-        while (1) {
-            pid_t pid_valide = wait(&status);
-            ptrace(PTRACE_SETOPTIONS, pid_enfant, 0,PTRACE_O_EXITKILL | PTRACE_O_TRACEEXEC | PTRACE_O_TRACEFORK | PTRACE_O_TRACECLONE);
-            incrementer_compteurs(&compteur1, &compteur2, status);
-            numero_signal = trouver_numero_signal(&numero_signal, status);
-            if (WIFEXITED(status) && pid_valide == pid_enfant){
-                printf("%d\n", compteur2);
-                printf("%d", compteur1);
-                return WEXITSTATUS (status);
-            } else if (WIFSIGNALED(status)){
-                printf("%d\n", compteur2);
-                printf("%d", compteur1);
-                return 128 + WTERMSIG(status);
-            }
-            ptrace(PTRACE_CONT, pid_valide, 0, 0);
-        }
+        return executer_processus_parent(status, &compteur1, &compteur2, pid_enfant);
     }
 }
